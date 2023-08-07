6a06f97c58ca9a49a1ef8e86243236a9a40d0ffb
Alex <87733477+abcraftworx@users.noreply.github.com>
2023-08-07 21:03:51 +0200

    Add files via upload

--- VariableTypes.ps1
+++ VariableTypes.ps1
@@ -1,51 +1,76 @@
-﻿# variable types
+﻿cls
+# variable types
 
 $a = 0
-($a.GetType()).Name
-Write-Host $a -ForegroundColor Blue
+Write-Host "Variabletype of "'$a'" is $(($a.GetType()).Name), Variable value: $a" -ForegroundColor White
+Write-Host ""
 
 $b = '0'
-($b.GetType()).Name
-Write-Host $b -ForegroundColor Cyan
+Write-Host "Variabletype of "'$b'" is $(($b.GetType()).Name), Variable value: $b" -ForegroundColor Cyan
+Write-Host ""
 
 $c = "0"
-($c.GetType()).Name
-Write-Host $c -ForegroundColor Red
+Write-Host "Variabletype of "'$c'" is $(($c.GetType()).Name), Variable value: $c" -ForegroundColor Red
+Write-Host ""
 
 $d = 1..10
-($d.GetType()).Name
-Write-Host $d -ForegroundColor Green
+Write-Host "Variabletype of "'$d'" is $(($d.GetType()).Name), Variable value: $d" -ForegroundColor Yellow
+Write-Host ""
 
 $e = (1..10)
-($e.GetType()).Name
-Write-Host $e -ForegroundColor Yellow
-
-#------------------------------------
+Write-Host "Variabletype of "'$e'" is $(($e.GetType()).Name), Variable value: $e" -ForegroundColor Green
+Write-Host ""
 
 [string]$f = 0
-($f.GetType()).Name
-Write-Host $f -ForegroundColor DarkBlue
+Write-Host "Variabletype of "'$f'" is $(($f.GetType()).Name), Variable value: $f" -ForegroundColor Blue
+Write-Host ""
 
 [int]$g = '0'
-($g.GetType()).Name
-Write-Host $g -ForegroundColor DarkCyan
+Write-Host "Variabletype of "'$g'" is $(($g.GetType()).Name), Variable value: $g" -ForegroundColor White
+Write-Host ""
 
-[int]$h = "0"
-($h.GetType()).Name
-Write-Host $h -ForegroundColor DarkRed
+[double]$h = "0"
+Write-Host "Variabletype of "'$h'" is $(($h.GetType()).Name), Variable value: $h" -ForegroundColor Yellow
+Write-Host ""
 
-$i = 1..10
-($i.GetType()).Name
-Write-Host $i -ForegroundColor DarkGreen
+$i = 1..5
+Write-Host "Variabletype of "'$i'" is $(($i.GetType()).Name), Variable value: $i" -ForegroundColor Cyan
+Write-Host ""
 
-$j = (1..10)
-($j.GetType()).Name
-Write-Host $j -ForegroundColor DarkYellow
+$j = (1..5)
+Write-Host "Variabletype of "'$j'" is $(($j.GetType()).Name), Variable value: $j" -ForegroundColor Gray
+Write-Host ""
 
-foreach ( $unit in $i ){
-    Write-Host $unit -ForegroundColor DarkMagenta
+[array]$k = 1, 2
+Write-Host "Variabletype of "'$k'" is $(($k.GetType()).Name), Variable value: $k" -ForegroundColor Cyan
+Write-Host ""
+
+[String]$l = (1..5)
+Write-Host "Variabletype of "'$l'" is $(($l.GetType()).Name), Variable value: $l" -ForegroundColor Gray
+Write-Host ""
+
+Write-Host "Loops" -ForegroundColor Blue -BackgroundColor White
+
+foreach ( $uniti in $i ){
+    Write-Host $uniti -ForegroundColor Yellow
+    Write-Host "Variabletype of "'$uniti'" is $(($uniti.GetType()).Name), Variable value: $uniti" -ForegroundColor Yellow
+    Write-Host ""
 }
 
-foreach ( $unit in $j ){
-    Write-Host $unit -ForegroundColor Magenta
+foreach ( $unitj in $j ){
+    Write-Host $unitj -ForegroundColor Magenta
+    Write-Host "Variabletype of "'$unitj'" is $(($unitj.GetType()).Name), Variable value: $unitj" -ForegroundColor Magenta
+    Write-Host ""
+}
+
+foreach ( $unitk in $k ){
+    Write-Host $unitk -ForegroundColor White
+    Write-Host "Variabletype of "'$unitk'" is $(($unitk.GetType()).Name), Variable value: $unitk" -ForegroundColor White
+    Write-Host ""
+}
+
+foreach ( $unitl in $l ){
+    Write-Host $unitk -ForegroundColor Red
+    Write-Host "Variabletype of "'$unitl'" is $(($unitl.GetType()).Name), Variable value: $unitl" -ForegroundColor Red
+    Write-Host ""
 }
